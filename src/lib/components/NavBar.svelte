3bd8d99900b97c994e1e8e78b56e24b5b26d96ed
jlwoolf <jlwoolf@proton.me>
2024-06-07 22:54:33 -0600

    created logo

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { Icon, PlusCircle } from 'svelte-hero-icons';
+	import { Icon, PlusCircle, Film } from 'svelte-hero-icons';
 	import SearchBar from './SearchBar.svelte';
 	import AddModal from './AddModal.svelte';
 	import type { MovieType } from '$lib/db/movie';
+	import { base } from '$app/paths';
 
 	export let movies: MovieType[];
 
@@ -12,15 +13,20 @@
 	};
 </script>
 
-<div class="sticky top-0 navbar bg-base-200 px-2 z-[1] w-full">
-	<div class="flex-1 gap-2">
+<div class="navbar sticky top-0 z-[1] w-full bg-base-200 px-2">
+	<div class="flex-none gap-2">
 		<!-- <SearchBar bind:movies/> -->
+		<img src="{base}/favicon.svg" class="w-12 h-12" alt="Movie Logo"/>
 	</div>
-	<!-- <span class="flex-1">
-		Movie Night
-	</span> -->
+	<div class="w-full flex-1 items-center justify-center">
+		<h1 class="text-lg font-bold">Movie Night Manager</h1>
+	</div>
 	<div class="flex-none">
-		<button class="btn btn-primary tooltip-left h-12 w-12 justify-end border-0 tooltip" on:click={addButtonOnClick} data-tip="Add Movie">
+		<button
+			class="btn btn-primary tooltip tooltip-left h-12 w-12 justify-end border-0"
+			on:click={addButtonOnClick}
+			data-tip="Add Movie"
+		>
 			<Icon src={PlusCircle} />
 		</button>
 	</div>
